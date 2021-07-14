650b883a2bd14b979f01d92a171e19d4f3a029e7
NikolaiV3rm <87257842+NikolaiV3rm@users.noreply.github.com>
2021-07-15 10:54:49 +1200

    Update The Final Stand 2 Infinite Stamina.lua

--- The Final Stand 2 Infinite Stamina.lua
+++ The Final Stand 2 Infinite Stamina.lua
@@ -1,5 +1,5 @@
 local oldNewIndex
-oldNewIndex = hookmetamethod(game, "__newindexx", function(Object, Property, Value)
+oldNewIndex = hookmetamethod(game, "__newindex", function(Object, Property, Value)
     if string.find(tostring(Object), "Stamina") and tostring(Object) ~= "StaminaServer" and tostring(Property) == "Value" then
        return oldNewIndex(Object, Property, 100)
     end
