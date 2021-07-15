fce6a56559c4210cda9b5654305c7d68843426d3
NikolaiV3rm <87257842+NikolaiV3rm@users.noreply.github.com>
2021-07-15 20:56:54 +1200

    Delete The Final Stand 2 Infinite Stamina.lua

--- The Final Stand 2 Infinite Stamina.lua
+++
@@ -1,7 +0,0 @@
-local oldNewIndex
-oldNewIndex = hookmetamethod(game, "__newindex", function(Object, Property, Value)
-    if string.find(tostring(Object), "Stamina") and tostring(Object) ~= "StaminaServer" and tostring(Property) == "Value" then
-       return oldNewIndex(Object, Property, 100)
-    end
-    return oldNewIndex(Object, Property, Value)
-end)
